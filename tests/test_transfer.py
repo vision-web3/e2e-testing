0168089c0f668dad7ec3fe6a4b98e31ef7b26c3a
Danut Ilisei <danut.ilisei@bitpanda.com>
2025-05-28 10:58:59 +0200

    test timeout

--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -7,29 +7,40 @@ import vision.client as pc
 from vision.client.library.entitites import DestinationTransferStatus
 from vision.common.servicenodes import ServiceNodeTransferStatus
 
-
 from helper import configure_client, configure_nodes, teardown_environment
 from helper import wait_for_service_node_to_be_ready
 from helper import check_service_nodes
 from conftest import configure_existing_environment
 
-test_timeout = int(os.getenv('PYTEST_TIMEOUT', 300))  # 5 minutes
+test_timeout = int(os.getenv('PYTEST_TIMEOUT', 1000))  # 5 minutes
+
 
 @pytest.fixture(scope="module", autouse=True)
-def setup_module(request, stack_id, worker_id = "gw0"):
+def setup_module(request, stack_id, worker_id="gw0"):
     # Code to set up the environment before any tests in the module
     print("Setting up module environment")
     #os.environ['VISION_ENV_FILE'] = '/Users/joe.matthew/Workspace/cd-workflows/tests/testnet/e2e-test-config.env'
     #os.environ['ETHEREUM_PRIVATE_KEY'] = '{"address":"f5454fd5b6aac3d243226cec31489c507a8259e0","crypto":{"cipher":"aes-128-ctr","ciphertext":"65a77c0acf155567086e1f231194ac13556e1148aa5b2170f509c679ccc8d68f","cipherparams":{"iv":"aa61677cf5316fcf7f514bc8db8d6528"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"cc9be3fe948e1c46cee2a86082c4db868c3f86fe4b4953f56c2c2624675e9d2e"},"mac":"7f73f4aeda2025086eb608ab360ca5b763d846aec2c331be069e42676ba25241"},"id":"712ce8d5-0ed5-4d32-83a4-0d9b1fa8a290","version":3}'
     #os.environ['BNB_CHAIN_PRIVATE_KEY'] = '/Users/joe.matthew/Workspace/e2e-testing/keystore'
     if os.getenv('VISION_ENV_FILE', None) is None:
-        port_offset = int(worker_id.replace("gw", "")) if worker_id != "master" else 0
+        port_offset = int(worker_id.replace(
+            "gw", "")) if worker_id != "master" else 0
         #os.environ['PORT_OFFSET'] = str(port_offset)
-        configure_nodes({
-            'ethereum_contracts': {'args': '', 'port_group': port_offset},
-            'service_node': {'instance_count': 1, 'port_group': port_offset},
-            'validator_node': {'instance_count': 1, 'port_group': port_offset}
-        }, stack_id)
+        configure_nodes(
+            {
+                'ethereum_contracts': {
+                    'args': '',
+                    'port_group': port_offset
+                },
+                'service_node': {
+                    'instance_count': 1,
+                    'port_group': port_offset
+                },
+                'validator_node': {
+                    'instance_count': 1,
+                    'port_group': port_offset
+                }
+            }, stack_id)
         configure_client(stack_id)
         request.addfinalizer(lambda: teardown_environment(stack_id))
     else:
@@ -43,22 +54,23 @@ def setup_module(request, stack_id, worker_id = "gw0"):
 def test_retrieve_token_balance(receiving_address):
     try:
         token_balance = pc.retrieve_token_balance(
-            pc.Blockchain.ETHEREUM,
-            pc.BlockchainAddress(receiving_address),
+            pc.Blockchain.ETHEREUM, pc.BlockchainAddress(receiving_address),
             pc.TokenSymbol('vsn'))
         assert token_balance is not None
         print(f'Token balance: {token_balance}')
     except pc.VisionClientError:
         pytest.fail("VisionClientError raised")
 
+
 def test_retrieve_service_node_bids():
     try:
-        service_node_bids = pc.retrieve_service_node_bids(pc.Blockchain.ETHEREUM,
-                                                          pc.Blockchain.BNB_CHAIN, False)
+        service_node_bids = pc.retrieve_service_node_bids(
+            pc.Blockchain.ETHEREUM, pc.Blockchain.BNB_CHAIN, False)
         assert service_node_bids != {}
         print(f'Service node bids: {service_node_bids}')
     except pc.VisionClientError:
         pytest.fail("VisionClientError raised")
+
 
 @pytest.mark.timeout(test_timeout)
 @pytest.mark.parametrize('keystore', ['eth'], indirect=True)
@@ -67,8 +79,8 @@ def test_token_transfer(receiving_address, private_key):
     try:
         token_transfer_response = pc.transfer_tokens(
             pc.Blockchain.ETHEREUM, pc.Blockchain.BNB_CHAIN, private_key,
-            pc.BlockchainAddress(receiving_address),
-            pc.TokenSymbol('vsn'), decimal.Decimal('0.000001'))
+            pc.BlockchainAddress(receiving_address), pc.TokenSymbol('vsn'),
+            decimal.Decimal('0.000001'))
         assert token_transfer_response is not None
         print(f'Token transfer response: {token_transfer_response}')
     except pc.VisionClientError:
@@ -78,18 +90,22 @@ def test_token_transfer(receiving_address, private_key):
     while not done:
         try:
             token_transfer_status = pc.get_token_transfer_status(
-                pc.Blockchain.ETHEREUM, token_transfer_response.service_node_address,
+                pc.Blockchain.ETHEREUM,
+                token_transfer_response.service_node_address,
                 token_transfer_response.task_id)
             assert token_transfer_status is not None
             print(f'Token transfer status: {token_transfer_status}')
-            if (token_transfer_status.source_transfer_status is ServiceNodeTransferStatus.CONFIRMED
-                and token_transfer_status.destination_transfer_status is DestinationTransferStatus.CONFIRMED):
+            if (token_transfer_status.source_transfer_status
+                    is ServiceNodeTransferStatus.CONFIRMED
+                    and token_transfer_status.destination_transfer_status
+                    is DestinationTransferStatus.CONFIRMED):
                 done = True
             else:
                 print('Waiting for transfer to be confirmed...')
                 time.sleep(5)
         except pc.VisionClientError:
             pytest.fail("VisionClientError raised")
+
 
 # Enable when we can start the token creator
 # @pytest.mark.parametrize('keystore', ['eth'], indirect=True)
@@ -105,4 +121,4 @@ def test_token_transfer(receiving_address, private_key):
 #         assert task_id is not None
 #         print(f'Task ID deployment: {task_id}')
 #     except pc.VisionClientError:
-#         pytest.fail("VisionClientError raised")    
+#         pytest.fail("VisionClientError raised")
